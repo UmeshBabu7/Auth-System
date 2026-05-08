05387f5db7b6e0165d7bf0d57b6dfb4a1dcdde23
UmeshBabu7 <umeshtamang129@gmail.com>
2026-05-08 08:18:20 +0545

    feat: add initial migration for custom user model

--- backend/users/models/custom_user.py
+++ backend/users/models/custom_user.py
@@ -1,8 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from users.managers import CustomUserManager
 
 
 class CustomUser(AbstractUser):
     USERNAME_FIELD = "email"
     email = models.EmailField(unique=True)
     REQUIRED_FIELDS = []
+    username = None
+
+    objects = CustomUserManager()
