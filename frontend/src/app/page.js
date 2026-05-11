9cfff29ecac44351dbd2f02b6b36056d12ad54d3
UmeshBabu7 <umeshtamang129@gmail.com>
2026-05-11 13:22:34 +0545

    fix: add try/catch error handling

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -7,9 +7,13 @@ export default function Home() {
 
   useEffect(() => {
     const getUser = async () => {
-      const userDetails = await getUserInfo();
-      if (userDetails) {
-        setUser(userDetails);
+      try {
+        const userDetails = await getUserInfo();
+        if (userDetails) {
+          setUser(userDetails);
+        }
+      } catch (e) {
+        console.log("Not logged in");
       }
     };
     getUser();
@@ -26,17 +30,20 @@ export default function Home() {
   return (
     <div className="min-h-screen bg-gray-100 items-center flex flex-col justify-center">
       <div className="bg-gray-600 p-8 flex flex-col rounded-lg">
-      {user ? <h1>Hi, {user.username}</h1> : <h1>Welcome stranger!</h1>}
-      <button className="bg-blue-400 p-1 rounded-sm m-1" onClick={handleLogout}>
-        Logout
-      </button>
-      <button
-        className="bg-blue-400 p-1 rounded-sm m-1"
-        onClick={handleRefresh}
-      >
-        refresh token
-      </button>
-    </div>
+        {user ? <h1>Hi, {user.username}</h1> : <h1>Welcome stranger!</h1>}
+        <button
+          className="bg-blue-400 p-1 rounded-sm m-1"
+          onClick={handleLogout}
+        >
+          Logout
+        </button>
+        <button
+          className="bg-blue-400 p-1 rounded-sm m-1"
+          onClick={handleRefresh}
+        >
+          refresh token
+        </button>
+      </div>
     </div>
   );
 }
